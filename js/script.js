c7c968cc0fde9f51872f5a0349e186bf531fa2a5
Saman <samanpreet.pannu1126@gmail.com>
2022-04-17 14:40:19 -0700

    Add

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var line = document.getElementById("line");
+var length = line.getTotalLength();
+line.style.strokeDasharray = length;
+line.style.strokeDashoffset = length;
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  var scrollpercent =
+    (document.body.scrollTop + document.documentElement.scrollTop) /
+    (document.documentElement.scrollHeight -
+      document.documentElement.clientHeight);
+
+  var draw = length * scrollpercent;
+  line.style.strokeDashoffset = length - draw;
+}
+
